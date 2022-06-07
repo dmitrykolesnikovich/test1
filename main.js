eaef294b2b16ee158c5cbb605a07d404c6a4bd8f
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2022-06-07 12:46:14 +0300

    Refactoring.

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
 
 // 2.
 const video = document.createElement("video");
-video.setAttribute('muted', 'muted');
+/* video.setAttribute('muted', 'muted');
 video.setAttribute('playsinline', 'playsinline');
 video.setAttribute('webkit-playsinline', 'webkit-playsinline');
-video.setAttribute('preload', 'auto');
-video.crossOrigin = "anonymous";
+video.setAttribute('preload', 'auto'); */
+video.autoplay = true;
+video.muted = true;
+video.src = 'cta.mp4';
+// video.crossOrigin = "anonymous";
 video.style.width = '400px';
 video.style.height = '400px';
 video.style.zIndex = '100';
@@ -17,14 +20,14 @@ video.style.left = '0px';
 video.style.top = '0px';
 document.body.appendChild(video);
 
-const url = 'cta.mp4';
-const xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
-xhr.onload = function() {
-    video.src = url;
-    video.load();
-};
-xhr.send();
+// const url = 'cta.mp4';
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', url, true);
+// xhr.onload = function() {
+//     video.src = url;
+//     video.load();
+// };
+// xhr.send();
 
 window.addEventListener('mousedown', () => video.play());
 window.addEventListener('touchstart', () => video.play());

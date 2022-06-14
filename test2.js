547935714bbf7ac507788bc8de19b48d9c545084
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2022-06-14 16:55:48 +0300

    Refactoring.

--- test2.js
+++ test2.js
@@ -12,14 +12,6 @@ video.style.position = 'absolute';
 video.style.left = '0px';
 video.style.top = '0px';
 
-video.addEventListener('timeupdate', function video_time_update(event) {    
-    if (video.currentTime > 0.1) {
-        video.removeEventListener('timeupdate', video_time_update);
-        video.pause();
-        video.currentTime = 0;        
-    }
-});
-
 document.body.appendChild(video);
 
 window.addEventListener('mousedown', () => video.play());
